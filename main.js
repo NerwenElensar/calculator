1bf3ab14215aad9ef5b5c449b146bb0344593d80
Nerwen <jenny2209@gmx.de>
2022-05-05 11:22:07 +0200

    add eventlisteners with no functionality

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+// collect the data here to use it later with the operate function
+const expression = {
+  firstOperand: "",
+  secondOperand: "",
+  operator: "",
+};
+
+//Eventlisteners
+const numberButtons = Array.from(document.querySelectorAll(".number"));
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(e.target.textContent);
+  });
+});
+
+const operatorButtons = Array.from(document.querySelectorAll(".operator"));
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(e.target.textContent);
+  });
+});
+
+const equalsButton = document.querySelector(".equals");
+const clearButton = document.querySelector(".clear");
+equalsButton.addEventListener("click", (e) => {
+  console.log(e.target.textContent);
+});
+
+clearButton.addEventListener("click", (e) => {
+  console.log(e.target.textContent);
+});
+
+// General functions for calculations
+
 function add(a, b) {
   return a + b;
 }
