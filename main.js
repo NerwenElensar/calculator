92cb98024c8a11dc9d154f08a58f93594eb74b10
Nerwen <jenny2209@gmx.de>
2025-02-27 12:06:11 +0100

    implement operate function

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let firstOperand;
+let secondOperand;
+let operator;
+
 const add = function (a, b) {
   return a + b;
 };
@@ -11,4 +15,24 @@ const multiply = function (a, b) {
 };
 const divide = function (a, b) {
   return a / b;
+};
+
+const operate = function (firstOperand, secondOperand, operator) {
+  switch (operator) {
+    case "+":
+      return add(firstOperand, secondOperand);
+      break;
+    case "-":
+      return subtract(firstOperand, secondOperand);
+      break;
+    case "*":
+      return multiply(firstOperand, secondOperand);
+      break;
+    case "/":
+      return divide(firstOperand, secondOperand);
+      break;
+    default:
+      return "Not a valid operator. Choose +,-,*,/";
+      break;
+  }
 };
