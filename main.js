4809a40b8fc64616d81187587b7aa98dded53ac9
Nerwen <jenny2209@gmx.de>
2025-03-11 17:29:29 +0100

    fix bug and add divided by zero error screen

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let firstOperand;
 let secondOperand;
 let operator;
-let usedEqualsBefore = false;
+const zeroErrorEmoji = "ಠ_ಠ div0";
 
 const numberButtons = document.querySelectorAll(".number");
 const operatorButtons = document.querySelectorAll(".operator");
@@ -18,8 +18,8 @@ numberButtons.forEach((number) => {
 
 operatorButtons.forEach((op) => {
   op.addEventListener("click", (e) => {
-    // set operator to input if firstOperand
-    if (firstOperand) {
+    // set operator to input if firstOperand, but no second operand to prevent overwriting
+    if (firstOperand && !secondOperand) {
       operator = e.target.textContent;
       console.log(`Operator set: ${operator}`);
     }
@@ -32,6 +32,9 @@ operatorButtons.forEach((op) => {
       console.log(`Result: ${firstOperand}`);
       secondOperand = "";
       operator = e.target.textContent;
+      console.log(`OpSetSecIF: ${operator}`);
+      //reset calc, if divided by zero error
+      checkForZeroDivisionAndAlert(firstOperand);
     }
   });
 });
@@ -46,7 +49,8 @@ equalsButton.addEventListener("click", (e) => {
     console.log(`Result: ${firstOperand}`);
     secondOperand = "";
     operator = e.target.textContent;
-    usedEqualsBefore = true;
+    checkForZeroDivisionAndAlert(firstOperand);
+    //usedEqualsBefore = true;
     // set result as firstOperand in case a second operation will happen
     // reset secondOperand (and operator???? clean state? easier to handle depends on other logic)
   }
@@ -61,6 +65,7 @@ function resetCalculator() {
   firstOperand = "";
   secondOperand = "";
   operator = "";
+  document.querySelectorAll("button").forEach((btn) => (btn.disabled = false));
   console.log(
     `RESETFunc first second operator: ${firstOperand} ${secondOperand} ${operator} `
   );
@@ -71,15 +76,14 @@ function showResultInDisplay(result) {
 }
 
 function showInDisplay(number) {
-  // Remove trailing 0, clean before entering second number and start new calc when entering new number after prev calculation
   if (
     displayUI.textContent == 0 ||
     (firstOperand && operator && !secondOperand) ||
-    usedEqualsBefore
+    usedEqualsBefore()
   ) {
-    if (usedEqualsBefore) {
-      usedEqualsBefore = false;
-      resetCalculator();
+    if (usedEqualsBefore()) {
+      //usedEqualsBefore = false;
+      operator = "";
     }
     displayUI.textContent = "";
   }
@@ -96,6 +100,20 @@ function updateOperand(numberShownInDisplay) {
     firstOperand = numberShownInDisplay;
   }
   console.log(`Updated Operands: first:${firstOperand} sec: ${secondOperand}`);
+}
+
+function usedEqualsBefore() {
+  return operator === "=";
+}
+
+function checkForZeroDivisionAndAlert(operand) {
+  if (firstOperand == zeroErrorEmoji) {
+    resetCalculator();
+    showResultInDisplay(zeroErrorEmoji);
+    numberButtons.forEach((num) => (num.disabled = true));
+    operatorButtons.forEach((op) => (op.disabled = true));
+    equalsButton.disabled = true;
+  }
 }
 /* Calculator math functions */
 
@@ -126,7 +144,7 @@ function operate(firstOperand, secondOperand, operator) {
       return multiply(firstOperand, secondOperand);
       break;
     case "/":
-      return divide(firstOperand, secondOperand);
+      return secondOperand == 0 ? zeroErrorEmoji : divide(firstOperand, secondOperand);
       break;
     default:
       return "Not a valid operator. Choose +,-,*,/";
