0958af75752b73baa624b3084f019f2dd836980d
Nerwen <jenny2209@gmx.de>
2025-03-12 11:19:07 +0100

    remove console.logs and refactor

--- main.js
+++ main.js
@@ -18,46 +18,30 @@ numberButtons.forEach((number) => {
 
 operatorButtons.forEach((op) => {
   op.addEventListener("click", (e) => {
-    // set operator to input if firstOperand, but no second operand to prevent overwriting
     if (firstOperand && !secondOperand) {
       operator = e.target.textContent;
-      console.log(`Operator set: ${operator}`);
     }
-    // if secondOperand then do operation first show result,
-    // set result to firstOperand and reset operator and secondOperand
     if (secondOperand) {
-      console.log(`${firstOperand} ${operator} ${secondOperand}`);
       firstOperand = operate(+firstOperand, +secondOperand, operator);
-      showResultInDisplay(firstOperand);
-      console.log(`Result: ${firstOperand}`);
-      secondOperand = "";
-      operator = e.target.textContent;
-      console.log(`OpSetSecIF: ${operator}`);
-      //reset calc, if divided by zero error
-      checkForZeroDivisionAndAlert(firstOperand);
+      resetSecOperandAndSaveOperator(e.target.textContent);
+      showResultInDisplay();
+      checkForZeroDivisionAndAlert();
     }
   });
 });
 
 equalsButton.addEventListener("click", (e) => {
-  // does operation, when all variables set != undefined
   if (firstOperand && secondOperand && operator) {
-    // do operation and show result in display
-    console.log(`${firstOperand} ${operator} ${secondOperand}`);
     firstOperand = operate(+firstOperand, +secondOperand, operator);
-    showResultInDisplay(firstOperand);
-    console.log(`Result: ${firstOperand}`);
-    secondOperand = "";
-    operator = e.target.textContent;
-    checkForZeroDivisionAndAlert(firstOperand);
-    // set result as firstOperand in case a second operation will happen
-    // reset secondOperand (and operator???? clean state? easier to handle depends on other logic)
+    resetSecOperandAndSaveOperator(e.target.textContent);
+    showResultInDisplay();
+    checkForZeroDivisionAndAlert();
   }
 });
 
 clearButton.addEventListener("click", resetCalculator);
 
-/* Display logic */
+/* Helper Functions */
 
 function resetCalculator() {
   displayUI.textContent = "0";
@@ -65,13 +49,10 @@ function resetCalculator() {
   secondOperand = "";
   operator = "";
   document.querySelectorAll("button").forEach((btn) => (btn.disabled = false));
-  console.log(
-    `RESETFunc first second operator: ${firstOperand} ${secondOperand} ${operator} `
-  );
 }
 
-function showResultInDisplay(result) {
-  displayUI.textContent = String(result).substring(0, 9);
+function showResultInDisplay() {
+  displayUI.textContent = String(firstOperand).substring(0, 9);
 }
 
 function showInDisplay(number) {
@@ -97,17 +78,21 @@ function updateOperand(numberShownInDisplay) {
   } else {
     firstOperand = numberShownInDisplay;
   }
-  console.log(`Updated Operands: first:${firstOperand} sec: ${secondOperand}`);
 }
 
 function usedEqualsBefore() {
   return operator === "=";
 }
 
-function checkForZeroDivisionAndAlert(operand) {
+function resetSecOperandAndSaveOperator(currentOperator) {
+  secondOperand = "";
+  operator = currentOperator;
+}
+
+function checkForZeroDivisionAndAlert() {
   if (firstOperand == zeroErrorEmoji) {
     resetCalculator();
-    showResultInDisplay(zeroErrorEmoji);
+    showInDisplay(zeroErrorEmoji);
     numberButtons.forEach((num) => (num.disabled = true));
     operatorButtons.forEach((op) => (op.disabled = true));
     equalsButton.disabled = true;
